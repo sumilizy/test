7065dafedd5048102cd733acd10d8d8977af562b
user2 <user2@KH2>
2016-05-31 21:07:33 +0900

    test

---
+++ spring_2_example/src/main/java/com/goray/s2/HomeController.java
@@ -0,0 +1,58 @@
+package com.goray.s2;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+	@RequestMapping("/member/join")
+	public void goJoin(){	
+	}
+	
+	@RequestMapping("/member/joinProcess")
+	public String joinProcess(@ModelAttribute MemberDTO mdto){
+		/*MemberDTO mdto = new MemberDTO();*/
+		//mdto.setId(request.getParameter("id"));
+		//mdto.setPw(request.getParameter("pw"));
+		//mdto.setAge(age);
+		System.out.println(mdto.getId()+ mdto.getPw()+mdto.getAge());
+		return "redirect:/";
+	}
+	
+	
+}
---
+++ spring_2_example/src/main/webapp/WEB-INF/views/member/join.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<form action="joinProcess">
+		ID <input type="text" name="id"><br>
+		PW <input type="text" name="pw"><br>
+		Name <input type="text" name="name"><br>
+		Age <input type="text" name="age"><br>
+		<input type="submit" value="SIGN UP"><br>
+	</form>
+	 <a href="/">HOME</a>
+
+</body>
+</html>
